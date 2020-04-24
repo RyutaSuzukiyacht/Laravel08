9d55ff7a87581bb6c310fe0529a0f84221a55f99
”名前” <>
2020-04-24 12:24:48 +0000

    確認

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name ="viewport" content="width=device-width, initial-scale=1">
-    
-    <title>Mynews</title>
-  </head>
-  <body>
-    {{-- layouts/admin.blade.phpを読み込む --}}
+{{-- layouts/admin.blade.phpを読み込む --}}
 @extends('layouts.admin')
 
 
@@ -20,11 +10,8 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>ニュース新規作成</h2>
+                <h2>My プロフィール</h2>
             </div>
         </div>
     </div>
 @endsection
-    <h1>Myニュース作成画面</h1>
-  </body>
-</html>
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
 });
 
 //課題1
@@ -25,9 +25,15 @@ Route::group(['prefix' => 'admin'], function() {
 //課題2
 
 //課題3
-Route::get('XXX', 'AAAController@bbb');
+//Route::get('XXX', 'AAAController@bbb');
 
 //課題4
-Route::get('admin/profile/create', 'Admin\ProfileController@add');
+//Route::get('admin/profile/create', 'Admin\ProfileController@add');
 
-Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
+//Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('admin/profile/create', 'Admin\ProfileController@add')->middleware('auth');
+

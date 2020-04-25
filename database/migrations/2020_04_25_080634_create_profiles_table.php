e053f6e72d4c98f39c9a5779418f25009e5e39a4
”名前” <>
2020-04-25 08:32:16 +0000

    課題14

---
+++ database/migrations/2020_04_25_080634_create_profiles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name'); // ニュースのタイトルを保存するカラム
+            $table->string('gender');  // ニュースの本文を保存するカラム
+            $table->string('hobby');
+            $table->string('introductionphp');// 画像のパスを保存するカラム
+            $table->timestamps();
+        });
+        //
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+        //
+    }
+}
